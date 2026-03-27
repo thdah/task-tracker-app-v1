42b522bdce93f15b6a8d4b979b56535de9045959
thdah <tda4723@gmail.com>
2026-03-27 22:23:49 +0630

    initial commit

---
+++ task-tracker-app/src/main/java/com/task_tracker/TaskTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.task_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerAppApplication.class, args);
+	}
+
+}
